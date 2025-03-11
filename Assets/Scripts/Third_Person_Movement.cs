5b81f55e63622a2cf5c2b9f56c79d7743a79bc67
Lungky <lungkye@gmail.com>
2025-03-11 22:18:28 +0800

    Added third person camera

---
+++ Assets/Scripts/Third_Person_Movement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Third_Person_Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform cam; // Camera transform
+
+    public float speed = 3f; // Movement speed
+
+    public float turnSmoothTime = 0.1f; // Smooth time for turning
+
+    float turnSmoothVelocity; // Velocity for turning
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); // Get the horizontal input
+        float vertical = Input.GetAxisRaw("Vertical"); // Get the vertical input
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; // Normalize the direction vector so that if the player is moving diagonally, the speed is the same as moving in a straight line
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // Get the angle in degrees
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); // Smooth the angle
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f); // Rotate the player to the target angle
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; // Move the player in the direction of the target angle
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
